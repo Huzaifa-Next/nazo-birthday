7e0395ba3c5acb7e61d6f4bf0bcd2c3d32f89711
Huzaifa-Next <huzaifahameedd@gmail.com>
2026-09-15 20:57:53 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  vite: {
+    base: "/nazo-birthday/",
+  },
   tanstackStart: {
     server: { entry: "server" },
     spa: {
